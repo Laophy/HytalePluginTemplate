3d2e2ded78bc94b100db869984c91e2c44a3b734
Laophy <jpessman22@gmail.com>
2026-01-22 10:34:10 -0600

    init files

---
+++ src/main/java/com/LakeCountryGames/plugin/AutomationPlugin.java
@@ -0,0 +1,20 @@
+package com.LakeCountryGames.plugin;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class AutomationPlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public AutomationPlugin(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new GetVersion(this.getName(), this.getManifest().getVersion().toString()));
+        this.getCommandRegistry().registerCommand(new GetPlayerCommand());
+    }
+}
